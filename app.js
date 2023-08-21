a0a077212e20ba174a42e61279024b7fcb7ee63f
LimHongs <honggyuim2@gmail.com>
2023-08-22 02:01:12 +0900

    초기셋팅

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const routes = express.Router();
+const { AppDataSource } = require("./src/models/data-source");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('combined'));
+app.use(routes);
+app.get("/ping", (req, res, next) => {
+  res.status(200).json({ message: "pong" });
+});
+
+const startServer = async () => {
+  const PORT = process.env.PORT;
+
+  await AppDataSource.initialize();
+
+  app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`);
+  });
+};
+
+startServer();
