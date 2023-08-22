79d349ff743776fa471bd18ca0574fc5e8c55734
gyk0814 <49557580+gyk0814@users.noreply.github.com>
2023-08-22 12:20:33 +0900

    MODIFY: completed initial settings

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan");
 const cors = require("cors");
-const routes = express.Router();
+const morgan = require("morgan");
 const { AppDataSource } = require("./src/models/data-source");
+const { routes } = require("./src/routes");
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
-app.use(morgan('combined'));
+app.use(morgan("combined"));
+app.use(express.json());
+
 app.use(routes);
+
 app.get("/ping", (req, res, next) => {
   res.status(200).json({ message: "pong" });
 });
 
-const startServer = async () => {
-  const PORT = process.env.PORT;
-
-  await AppDataSource.initialize();
-
-  app.listen(PORT, () => {
-    console.log(`Listening on Port ${PORT}`);
-  });
-};
-
-startServer();
+const PORT = process.env.PORT;
+app.listen(PORT, async () => {
+  await AppDataSource.initialize()
+    .then(() => {
+      console.log("Data Source has been initialized!");
+    })
+    .catch((error) => {
+      console.error("Error during Data Source initialization", error);
+    });
+  console.log(`Listening to request on port: ${PORT}`);
+});
