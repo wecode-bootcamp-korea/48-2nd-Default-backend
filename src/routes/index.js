e94c374c94872d8945a829a8812a42afb470a0dd
dodoyoo <dong15903@daum.net>
2023-08-23 11:02:48 +0900

    db migration 수정3

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { userRouter } = require("./user.router");
-
-const routes = express.Router();
-
-routes.use("/user", userRouter);
-
-module.exports = { routes };
