b7c712b350537e085ba5e69c7a7c1dcd4f5c5524
dodoyoo <dong15903@daum.net>
2023-08-29 14:20:25 +0900

    reservation 2

--- src/models/reservation.dao.js
+++ src/models/reservation.dao.js
@@ -1,6 +1,12 @@
 const { AppDataSource } = require("./data-source")
 
-const creatReservation = async(userId, roomId, startDate, endDate) => {
+const isReservationImpossible = await checkReservationAvailability(roomId, startDate, endDate);
+
+if (!isReservationImpossible) {
+    throw new Error("Requested dates are not available for reservation");
+};
+
+const createReservation = async(userId, roomId, startDate, endDate) => {
      await AppDataSource.query (
         `INSERT INTO reservations (
             user_id,
@@ -18,4 +24,19 @@ const creatReservation = async(userId, roomId, startDate, endDate) => {
     );
 };
 
-module.exports = { creatReservation };
+const checkReservationAvailability = async (roomId, startDate, endDate) => {
+
+    const existingReservations = await AppDataSource.query (
+        'SELCET start_date, end_date FROM reservations WHERE room_id = ?',
+        [roomId]
+    );
+
+    for (const reservation of existingReservations) {
+        if (startDate < reservation.end_date && endDate > reservation.start_date) {
+            return false;
+        }
+    }
+    return true;
+}
+
+module.exports = { createReservation };
--- src/services/reservation.service.js
+++ src/services/reservation.service.js
@@ -1,8 +1,22 @@
 const reservationDao = require("../models/reservation.dao")
 
-const reservationBotton =  async (userId, roomId, startDate, endDate) => {
-   await reservationDao.creatReservation(userId, roomId, startDate, endDate);
-    
+const createReservation =  async (userId, roomId, startDate, endDate) => {
+   // 예약 가능한 날짜를 확인
+   
+   // if 예약 가능 -> createRservation
+   await reservationDao.createReservation(userId, roomId, startDate, endDate);
+    // if 가능하지 않으면 -> throw ERROR("INVALID DATE")
+    const isReservationImpossible = await checkReservationAvailability(roomId, startDate, endDate);
+
+if (!isReservationImpossible) {
+    throw new Error("Requested dates are not available for reservation");
 };
 
-module.exports = { reservationBotton };
+for (const reservation of existingReservations) {
+   if (startDate < reservation.end_date && endDate > reservation.start_date) {
+       return false;
+   }
+};
+}
+
+module.exports = { createReservation };
