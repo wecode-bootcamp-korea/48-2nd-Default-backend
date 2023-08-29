4e00769b0471a204f7f071887ec82c560b53cbff
dodoyoo <dong15903@daum.net>
2023-08-29 18:22:53 +0900

    DB column 추가

--- db/migrations/20230822031253_create_new_locations.sql
+++ db/migrations/20230822031253_create_new_locations.sql
@@ -1,7 +1,7 @@
 -- migrate:up
 CREATE TABLE locations (
   id int PRIMARY KEY NOT NULL auto_increment,
-  loacation_name varchar(255) NOT NULL  
+  location_name varchar(255) NOT NULL  
 );
 
 -- migrate:down
