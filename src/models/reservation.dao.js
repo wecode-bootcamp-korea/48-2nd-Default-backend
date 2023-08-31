9e75db9297fa33a77a781655fd7cb7affd18aa61
dodoyoo <dong15903@daum.net>
2023-08-31 17:06:44 +0900

    reservation 작성 4

--- src/models/reservation.dao.js
+++ src/models/reservation.dao.js
@@ -19,24 +19,40 @@ const createReservation = async(userId, roomId, startDate, endDate) => {
     );
 };
 
-const getExistingReservations = async(roomId, userStartDate, userEndDate) => {
-    const reservationResult = await AppDataSource.query (
-    `SELECT 
-        start_date, 
-        end_date 
-    FROM 
-        reservations 
-    WHERE 
-        room_id = ?
-    WHERE
-        start_date     
-        
-    ;
-    `,
-    [roomId]
-)
-return reservationResult;
-};
+// const getExistingReservations = async(roomId, startDate, endDate) => {
+//     const reservationResult = await AppDataSource.query (
+//     `SELECT 
+//         start_date as rstartDate, 
+//         end_date as rendDate
+//     FROM 
+//         reservations 
+//     WHERE
 
+//     ? = room_id  
+        
+//     ;
+//     `,
+//     [roomId]
+// )
+// return reservationResult;
+// };
+
+const getExistingReservations = async(roomId, startDate, endDate) => {
+    await AppDataSource.query (
+      `SELECT
+        start_date
+        end_date
+      FROM
+        reservations
+      WHERE
+        ? >=end_date OR
+        ? <=start_date AND
+        ? >= now() AND
+        ? >= now() AND
+        ? = room_id
+      `,
+      [startDate, endDate, startDate, endDate, roomId]
+    );
+  };
 
 module.exports = { createReservation, getExistingReservations};
--- src/services/reservation.service.js
+++ src/services/reservation.service.js
@@ -1,28 +1,43 @@
 const reservationDao = require("../models/reservation.dao")
 
-const createReservations = async(userId, roomId, startDate, endDate) => {
-    
+// const reservations = async (userId, roomId, startDate, endDate) => {
+//     const check = await reservationDao.getExistingReservations(roomId, startDate, endDate);
 
-    const checkReservationAvailability = await reservationDao.getExistingReservations(roomId)
-    console.log("예약 날짜 :",  checkReservationAvailability);
-    console.log(roomId);
-    const start = await reservationDao.createReservation;
-    if (!checkReservationAvailability) {
-        throw new Error("Requested dates are not available for reservation")
-    };
+//     console.log(check);
 
-    const { start_date, end_date } = checkReservationAvailability;
+//     for (let i = 0; i < check.length; i++) {
+//       if (check[i].rstartDate > endDate || check[i].rendDate < startDate) {
+//         const KST = 9 * 60 * 60 * 1000;
+//         startDate = startDate + KST
+//         endDate = endDate + KST
+//         await reservationDao.createReservation(userId, roomId, startDate, endDate);
+//       } else {
+//         const err = new Error("Invalid reservation");
+//         err.statuscode = 400;
+//         throw err;
+//       }
+//     }
+//   };
 
-    
-    for (i = 1; i<=start_date&&i>=end_date; i++ ) {
-        if (startDate < end_date && endDate > start_date) {
-            const err = new Error("Invalid Date");
-            err.statusCode = 400;
-            throw err;
-        }
-        return start;
-    };
-};
+const reservations =  async (userId, roomId, startDate, endDate) => {
+    const reservationCheck = await reservationDao.getExistingReservations(roomId, startDate, endDate);
+    const KST = 9 * 60 * 60 * 1000;
+    const today = '2023-09-01';
+    if(!reservationCheck) {
+     if (startDate > today && endDate > today) {
+       await reservationDao.createReservation(userId, roomId, startDate, endDate); }
+     else {
+       const err = new Error("The reservation date cannot be earlier than the current date.");
+       err.statuscode = 400;
+       throw err;
+     }
+    } else {
+     const err = new Error("The reservation date overlaps with the current date");
+     err.statuscode = 400;
+     throw err;
+    }
+ };
+ 
 
 
-module.exports = { createReservations };
+module.exports = { reservations };
